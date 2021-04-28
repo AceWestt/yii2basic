1462a0146cc28e6683b75bc1decdf5db2b252bef
Asilbek khamzaev <asilbek.khamzaev@AKFA.GROUP>
2021-04-28 15:57:23 +0500

    start

---
+++ engine/controllers/AppController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Администратор
+ * Date: 06.03.2020
+ * Time: 13:15
+ */
+
+namespace app\controllers;
+
+
+use yii\web\Controller;
+
+class AppController extends Controller
+{
+
+}
